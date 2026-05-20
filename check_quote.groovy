85a8d418e3cee7578c478851b63be07a1f325e71
Jonas Ribeiro <krnsjrr@gmail.com.br>
2026-05-20 08:55:06 -0300

    chore: remove unused groovy script

--- check_quote.groovy
+++
@@ -1,7 +0,0 @@
-import org.apache.ofbiz.entity.DelegatorFactory
-import org.apache.ofbiz.entity.Delegator
-import org.apache.ofbiz.entity.model.ModelEntity
-
-Delegator delegator = DelegatorFactory.getDelegator("default")
-ModelEntity modelQuote = delegator.getModelReader().getModelEntity("Quote")
-println "Fields of Quote: " + modelQuote.getAllFieldNames()
